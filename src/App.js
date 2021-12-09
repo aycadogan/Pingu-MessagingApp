46628cc71d9485e151f6bc9da4d99d9879152a42
aycadogan89 <doganayca89@gmail.com>
2021-12-08 23:26:15 -0800

    login and firebase setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      {/* main or login */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
